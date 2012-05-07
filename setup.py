f33ea7ad0fa3976f2c4da33133bf07bd17a3ea52
Daniel Robbins <drobbins@funtoo.org>
2012-05-07 16:45:08 +0000

    GPLv2 licensing fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 NAME = "ZenPacks.community.OpenSolaris"
 VERSION = "1.1"
 AUTHOR = "Eric Edgar"
-LICENSE = ""
+LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
 PACKAGES = ['ZenPacks', 'ZenPacks.community', 'ZenPacks.community.OpenSolaris']
 INSTALL_REQUIRES = []
